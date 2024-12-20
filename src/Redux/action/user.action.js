25eb558b62bdcdbf72c03d7b61d9ad642d09882b
Zulfahluthfianafathiyah <zulfahluthfiana@gmail.com>
2024-12-20 20:35:15 +0700

    cobain

--- src/Redux/action/user.action.js
+++ src/Redux/action/user.action.js
@@ -13,7 +13,7 @@ export const createPelanggan = (pelanggan) => async (dispatch) => {
 
     const data = await response.json();
     console.log(data);
-    window.location.href = "/login";
+    window.location.href = "/";
     dispatch({
       type: pelangganActions.CREATE_PELANGGAN_SUCCESS,
       payload: data,
